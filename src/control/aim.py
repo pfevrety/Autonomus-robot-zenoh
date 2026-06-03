9cc1b004c78f8c03922c8e4923bd2a06ccd36dae
eytan.benharrous <eytan.benharrous@student-cs.fr>
2026-06-03 20:41:30 +0200

    debug

--- src/control/aim.py
+++ src/control/aim.py
@@ -21,12 +21,12 @@ class Aim:
         self.session = zenoh.open(conf)
 
         self.sub = self.session.declare_subscriber("robot/aimed", self.box_callback)
-        self.sub2 = self.session.declare_subscriber(
-            "robot/not_aimed", self.not_aimed_callback
-        )
-        self.sub3 = self.session.declare_subscriber(
-            "robot/nothing_to_aimed", self.nothing_to_aimed_callback
-        )
+        # self.sub2 = self.session.declare_subscriber(
+        #     "robot/not_aimed", self.not_aimed_callback
+        # )
+        # self.sub3 = self.session.declare_subscriber(
+        #     "robot/nothing_to_aimed", self.nothing_to_aimed_callback
+        # )
 
         self.sub_lat = self.session.declare_subscriber(
             "robot/config/latency", self.latency_callback
@@ -48,37 +48,37 @@ class Aim:
 
     def box_callback(self, sample: zenoh.Sample):
         current_time = time.time()
-        print("AIIIIIMED")
+        # print("AIIIIIMED")
 
-        self.last_time = current_time
+        # self.last_time = current_time
 
-        self.aimed_time = time.time()
-        if current_time - self.last_action_time < self.latency:
-            self.aimed = 0.5
-            return
+        # self.aimed_time = time.time()
+        # if current_time - self.last_action_time < self.latency:
+        #     self.aimed = 0.5
+        #     return
 
-        self.last_action_time = current_time
+        # self.last_action_time = current_time
         data = json.loads(sample.payload.to_bytes())
         self.aimed = data.get("normalized_center")[0]
-        print("AIIIIIMED", self.aimed, data.get("normalized_center")[0])
+        # print("AIIIIIMED", self.aimed, data.get("normalized_center")[0])
 
-    def not_aimed_callback(self, sample: zenoh.Sample):
-        current_time = time.time()
-        self.last_time = current_time
-        if current_time - self.aimed_time > 0.1:
-            return
-        if 0.05 < current_time - self.last_action_time < self.latency:
-            self.aimed = 0.5
-            return
-        print("NOOOOOOOOOOOOO")
-        self.last_action_time = current_time
-        self.aimed = 0.9
+    # def not_aimed_callback(self, sample: zenoh.Sample):
+    #     current_time = time.time()
+    #     self.last_time = current_time
+    #     if current_time - self.aimed_time > 0.1:
+    #         return
+    #     if 0.05 < current_time - self.last_action_time < self.latency:
+    #         self.aimed = 0.5
+    #         return
+    #     print("NOOOOOOOOOOOOO")
+    #     self.last_action_time = current_time
+    #     self.aimed = 0.9
 
-    def nothing_to_aimed_callback(self, sample: zenoh.Sample):
-        current_time = time.time()
-        self.last_time = current_time
-        print("nta")
-        self.aimed = 0.5
+    # def nothing_to_aimed_callback(self, sample: zenoh.Sample):
+    #     current_time = time.time()
+    #     self.last_time = current_time
+    #     print("nta")
+    #     self.aimed = 0.5
 
     def pub_twist(self, linear, angular):
         t = Twist(
@@ -94,16 +94,19 @@ class Aim:
 
         if abs(self.aimed - 0.5) <= self.deadzone / 2.0:
             self.pub_twist(0.0, 0.0)
+            print("move 0,0")
         else:
             intensity = (abs(self.aimed - 0.5) - self.deadzone / 2.0) / (
                 0.5 - self.deadzone / 2.0
             )
-            print(intensity)
+            # print(intensity)
 
             if self.aimed > 0.5:
                 self.pub_twist(0.0, intensity * self.angular_scale)
+                print("move 0,", intensity * self.angular_scale)
             else:
                 self.pub_twist(0.0, -intensity * self.angular_scale)
+                print("move 0,", -intensity * self.angular_scale)
 
     def destroy(self):
         # On nettoie proprement les souscriptions
