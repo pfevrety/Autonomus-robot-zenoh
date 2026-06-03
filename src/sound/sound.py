113cb53b493c56163ce3d34aa3a094db2f775a62
eytan.benharrous <eytan.benharrous@student-cs.fr>
2026-06-03 09:24:08 +0200

    sound function

--- src/sound/sound.py
+++ src/sound/sound.py
@@ -17,27 +17,30 @@ if servo is None:
     print("[WARN] Unable to connect to OpenCR.")
 else:
     servo.write1ByteTxRx(IMU_RE_CALIBRATION, 1)
-
-time.sleep(3.0)
-
 print("[INFO] Running!")
-if servo is not None:
+
+
+def play_sound(sound_id):
+    if servo is not None:
+        servo.write1ByteTxRx(HEARTBEAT, 0)
+        servo.write4ByteTxRx(SOUND, sound_id)
+
+
+def play_on():
     print("Play ON")
-    servo.write1ByteTxRx(HEARTBEAT, 0)
-    servo.write4ByteTxRx(SOUND, 1)
-    time.sleep(5)
+    play_sound(1)
 
-    print("Play LOW_BATTERY")
-    servo.write1ByteTxRx(HEARTBEAT, 0)
-    servo.write4ByteTxRx(SOUND, 2)
-    time.sleep(5)
 
+def play_error():
     print("Play ERROR")
-    servo.write1ByteTxRx(HEARTBEAT, 0)
-    servo.write4ByteTxRx(SOUND, 3)
-    time.sleep(5)
+    play_sound(3)
 
+
+def play_bip():
+    print("Play Bip")
+    play_sound(3)
+
+
+def play_off():
     print("Play OFF")
-    servo.write1ByteTxRx(HEARTBEAT, 0)
-    servo.write4ByteTxRx(SOUND, 0)
-    time.sleep(5)
+    play_sound(0)
